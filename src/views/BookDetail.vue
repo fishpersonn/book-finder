8d794ff66feadcbe0f1f5e4bb2b1a915e8e8fc24
fisher <taiwanfishperson@gmail.com>
2025-09-21 16:07:43 +0800

    git commit -m "fix: 修正 BookDetail.vue 缺少內容的問題

---
+++ src/views/BookDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12" md="8">
+        <v-card class="mt-8">
+          <v-card-title class="text-h4 text-center">書籍詳細資訊</v-card-title>
+          <v-card-text class="text-center">
+            <p>這是一個用來顯示單一書籍詳細資訊的頁面。</p>
+            <p>你可以開始在這裡添加你的內容，例如書籍圖片、作者、描述等等。</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+// 在這裡加入這個頁面所需的邏輯，例如從 API 獲取書籍資料
+</script>
